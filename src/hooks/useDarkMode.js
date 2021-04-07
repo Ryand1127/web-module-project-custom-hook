607321626663d3f04f33bd81614ee8c2a41e01e1
Ryan Dill <ryand1127@gmail.com>
2021-04-07 15:29:43 -0400

    finished mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import {useLocalStorage} from './useLocalStorage'
+
+export const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', initialValue);
+
+    return [darkMode, setDarkMode]
+}
+
